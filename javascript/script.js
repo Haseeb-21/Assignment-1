8e03923ad71c233aa8b416066ec371fe4541b320
Haseeb <haseebmurtaza21@gmail.com>
2020-12-06 21:21:55 -0800

    Added saveNote() function to save to notesArray after pressing save button

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,20 @@
 document.querySelector('.btn2').onclick = darkTheme;   
 
-    function darkTheme() {
-        var a = document.querySelector('.list')
-        a.style.backgroundColor = "gray";
-        var b = document.querySelector('.notearea')
-        b.style.backgroundColor = "gray";
-        document.querySelector(".btn2").innerHTML = 'Light Theme';
+function darkTheme() {
+    var a = document.querySelector('.list')
+    a.style.backgroundColor = "gray";
+    var b = document.querySelector('.notearea')
+    b.style.backgroundColor = "gray";
+    document.querySelector(".btn2").innerHTML = 'Light Theme';
     }   
+
+let notesArray = [
+    {title:"note one", body:"some text 1"},
+    {title:"note two", body:"some text 2"}];
+
+document.querySelector('.btn3').onclick = saveNote;
+
+function saveNote() {
+    notesArray.push({title: "note three", body: "some text 3"});
+    console.log("Note saved successfully");
+}
